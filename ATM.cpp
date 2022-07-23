fb9a5cf0502d6f1c37ac19cc1ed5a68e7854db08
Sellasie <fsdjogatse@st.ug.edu.gh>
2022-07-23 15:50:40 +0000

    Create ATM.cpp

---
+++ ATM.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int pin
+    cout << "******Welcome to Reina Bank******" << endl;
+    
+    cout << "Insert your card"<<endl;
+    
+    cout << "PROCESSING..."<< endl;
+    
+    cout << "Enter your pin"<< endl;
+    cin >> pin;
+    cout << endl;
+    
+    cout << "PROCEED..."<< endl;
+    cout << "CHOOSE AN OPTION: "<< endl;
+    cout << endl;
+    cout << "1. Withdraw"<< endl;
+    cout << "2. Deposit" << endl;
+    cout << "3. Balance" << endl;
+    cout << "4. Exit" << endl;
+    
+}
