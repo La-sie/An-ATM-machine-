f85bf21cc4c34f86ea8b6de3b2a893bd86ebf364
Sellasie <fsdjogatse@st.ug.edu.gh>
2022-07-24 08:50:41 +0000

    Update ATM.cpp

--- ATM.cpp
+++ ATM.cpp
@@ -37,7 +37,14 @@ int main () {
 
        switch (optionNumber)
          {
-
+            case 1 :
+             break;
+            case 2 :
+             break;
+            case 3 :
+             break;
+            case 4 :
+             break;
          }
        }
 
